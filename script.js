ee5e88f5858aa25d353ce11451c7463c37a2f9bd
Aydin <aydinquliyev21@gmail.com>
2020-01-15 20:06:26 +0400

    blablablabla
    blablablablablablablabla

---
+++ script.js
@@ -0,0 +1,19 @@
+
+ 
+
+
+
+
+    function navbartoggle() {
+        var element = document.getElementById("togglediv");
+        var element = document.getElementById("navbar");
+        element.classList.toggle("active");
+     
+     }
+     
+        
+        
+
+
+
+     
